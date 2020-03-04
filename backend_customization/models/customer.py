4bbf573174a59c29a9e84b39f02c59d3b50b6945
jemankhalifa <khalifaeman4@gmail.com>
2020-03-04 18:22:16 +0300

    customer customization

--- backend_customization/models/customer.py
+++ backend_customization/models/customer.py
@@ -23,7 +23,7 @@ class ResPartner(models.Model):
     	context = self._context
 
     	res = super(ResPartner, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar,submenu=submenu)
-    	doc = etree.XML(res['arch'])
+    	arch = etree.XML(res['arch'])
     	if view_type == 'form':
     		print("<<<<<<<<<<<<<<<<<",self.state)
     		if context.get('search_default_customer', False) == 1 :
@@ -32,29 +32,11 @@ class ResPartner(models.Model):
                 for cu in custom_view:
     				print("_______________",cu)
 
-	    		for node in doc.xpath("//field"):   # All the view fields to readonly
+	    		for node in arch.xpath("//field"):   # All the view fields to readonly
 	    			print("RRRRRRRRRRRRRRRRRRRRRR",node)
-	    			node.set('readonly' ,'1')
+	    			
 	    			node.set('modifiers',simplejson.dumps("{'readonly':true}"))
 	    			print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$",node)
-	    		res['arch'] = etree.tostring(doc)
-    	# res = super(ResPartner,self).fields_view_get(view_id=view_id,view_type=view_type,toolbar=False,submenu=False)
-    	# print("11111111111111111111111",res)
-    	# if view_type == 'form':
-    	# 	res['arch'] = self.fields_view_get(res['arch'])
-    	# 	print("::::::::::::::::::::",res['arch'])
-    		# doc = etree.XML(res['arch'])
-    		# print("OOOOOOOOOOOOOOOOOOOOOOOOOO",doc)
-    		# nodes = doc.xpath("//form")
-    		# print("DSDSSSSSSSSSSSSSSSSSSSSSSSSSs",nodes)
-    		# for nod in nodes:
-    		# 	print("bbbbbbbbbbbbbbbbbb",nod)
-	    		#nod.set(n, '{"readonly": true}')
-	    		#{'name': {'readonly': False}}
+	    		res['arch'] = etree.tostring(arch)
 
-    		# for n in res['fields']:
-    		# 	f_attr = self.fields_get(n,'readonly')
-    		# 	print(">>>>>>>>>>>>>>>>>>>>>>>>>",f_attr)
-    		# 	f_attr.set(n, '{"readonly": true}')
-    		# 	print("xxxxxxxxxxxxxxxxx",n)
     	return res
